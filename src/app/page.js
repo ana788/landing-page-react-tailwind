4534fb9a57757c35cff63161f3287fb431d18774
ana788 <anabolinhu@outlook.com>
2023-09-01 22:06:46 -0300

    Add SectionServices na Page

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,13 @@
 import SectionMain from '@/components/SectionMain'
+import SectionServices from '@/components/SectionServices'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <SectionMain />
+    <>
+      <SectionMain />
+      <SectionServices />
+    </>
+
   )
 }
